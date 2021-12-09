63e1fb9244edb2053f9612e46e79230c3070ff23
Veronika Kukk <92779126+VeronikaKukk@users.noreply.github.com>
2021-12-09 10:11:55 +0200

    Add files via upload

--- parasiit.py
+++ parasiit.py
@@ -46,25 +46,31 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, type):
         super().__init__()
         # SIIA IF JA ELIF LAUSETE SISSE TULEVAD ERINEVAD VAENLASTE PILDID
-        if type == 'xxx':
-            xxx_frame1 = pygame.image.load(
-                os.path.join("XXX1.png")).convert_alpha()
-            xxx_frame2 = pygame.image.load(
-                os.path.join("XXX2.png")).convert_alpha()
-            self.frames = [xxx_frame1, xxx_frame2]
-        elif type == 'xx':
-            xx_frame1 = pygame.image.load(
-                os.path.join("XX1.png")).convert_alpha()
-            xx_frame2 = pygame.image.load(
-                os.path.join("XX2.png")).convert_alpha()
-            self.frames = [xx_frame1, xx_frame2]  # PILTIDE LIST
+        if type == 'porgand':
+            porgand_frame1 = pygame.image.load(
+                os.path.join("porgand.png")).convert_alpha()
+            porgand_frame2 = pygame.image.load(
+                os.path.join("porgand.png")).convert_alpha()
+            self.frames = [porgand_frame1, porgand_frame2]
+        elif type == 'kuuslauk':
+            kuuslauk_frame1 = pygame.image.load(
+                os.path.join("kuuslauk.png")).convert_alpha()
+            kuuslauk_frame2 = pygame.image.load(
+                os.path.join("kuuslauk.png")).convert_alpha()
+            self.frames = [kuuslauk_frame1, kuuslauk_frame2]  # PILTIDE LIST
+        elif type == 'ravim':
+            ravim_frame1 = pygame.image.load(
+                os.path.join("ravim.png")).convert_alpha()
+            ravim_frame2 = pygame.image.load(
+                os.path.join("ravim.png")).convert_alpha()
+            self.frames = [ravim_frame1, ravim_frame2]  # PILTIDE LIST
 
         self.animation_index = 0
         # VÕTAB ESIALGSEKS PILDIKS ESIMESE PILDI LISTIST
         self.image = self.frames[self.animation_index]
         # KUHU ENEMY SPAWNIB esimene on x ja teine y koordinaat
         self.rect = self.image.get_rect(
-            midbottom=(randint(0, 1), randint(0, 1)))
+           midbottom=(randint(1100, 1400), randint(150, 450)))
         # need koorinaadid tuleks panna suuremad, kui ekraanisuurus, sest enemy spawnib kaugemal ja liigub sealt ekraanile, mitte ei popupi järsku ekraanil
 
     def animation_state(self):
@@ -77,15 +83,12 @@ class Enemy(pygame.sprite.Sprite):
 
     def update(self):
         self.animation_state()
-        self.rect.x -= 5  # KUI KIIRESTI ENEMY LIIGUB EKRAANIL
+        self.rect.x -= 0.01  # KUI KIIRESTI ENEMY LIIGUB EKRAANIL
         self.destroy()  # KONTROLLIB, KAS VAJA HÄVITADA ENNAST
 
     def destroy(self):
-        if self.rect.x <= -100:  # see nr näitab mitu px ta ekraanilt väljas
+        if self.rect.x ==0:  # see nr näitab mitu px ta ekraanilt väljas
             self.kill()  # HÄVITAB ENNAST, KUI EKRAANILT VÄLJAS, muidu kui liiga palju siis jookseb mäng kokku
-
-# fn, et kui mäng läbi, siis enemy_group tehakse tühjaks, muidu spawniks uues mängus keset ekraani midagi
-# fn, kui mängija puudutab vaenlast, siis elu kaob ja fn sees peaks kontrollima kas elusid on piisavalt, et mäng edasi läheks
 
 
 class Verelible(pygame.sprite.Sprite):
@@ -93,9 +96,9 @@ class Verelible(pygame.sprite.Sprite):
         super().__init__()
         if type == "verelible":
             verelible_frame1 = pygame.image.load(
-                os.path.join("verelible.png")).convert_alpha()
+                os.path.join("verelible1.png")).convert_alpha()
             verelible_frame2 = pygame.image.load(
-                os.path.join("verelible.png")).convert_alpha()
+                os.path.join("verelible2.png")).convert_alpha()
             self.frames = [verelible_frame1, verelible_frame2]
 
         self.animation_index = 0
@@ -115,7 +118,7 @@ class Verelible(pygame.sprite.Sprite):
 
     def update(self):
         self.animation_state()
-        self.rect.x -= 0.3  # KUI KIIRESTI ENEMY LIIGUB EKRAANIL
+        self.rect.x -= 0.3  # KUI KIIRESTI  LIIGUB EKRAANIL
         self.destroy()  # KONTROLLIB, KAS VAJA HÄVITADA ENNAST
 
     def destroy(self):
@@ -165,7 +168,7 @@ ussike.add(Player(160, 100, 4, 'ussike'))
 # vaenlane
 enemy_group = pygame.sprite.Group()
 enemy_timer = pygame.USEREVENT + 0
-pygame.time.set_timer(enemy_timer, 1000)  # kui tihti enemy spawnib
+pygame.time.set_timer(enemy_timer, 2000)  # kui tihti enemy spawnib
 
 # taust
 background = pygame.image.load(os.path.join("aluminetaust.png")).convert()
@@ -300,11 +303,8 @@ while running:
         if event.type == verelible_timer:
             verelible_group.add(Verelible('verelible'))
             verelible_group.add(Verelible('verelible'))
-#         ////Kui enemy_timeri aeg on käes, siis valib randomilt vaenlase
-#         if event.type == enemy_timer:
-#             enemy_group.add(Enemy(choice(['xxx','xx']))) /////siia tulevad vaenlase nimed vms
-#         enemy_group.draw(screen)
-#         enemy_group.update()
+        if event.type == enemy_timer:
+            enemy_group.add(Enemy(choice(['porgand','kuuslauk','ravim'])))
 
     start_time = pygame.time.get_ticks()
 
@@ -317,7 +317,9 @@ while running:
 
     if pygame.sprite.spritecollide(ussike.sprite, verelible_group, True):
         score += 100
-
+    if pygame.sprite.spritecollide(ussike.sprite,enemy_group,True):
+        lives -= 1
+    
     score += start_time*0.000001
     score_txt = str(int(score))
 
@@ -328,12 +330,13 @@ while running:
         screen.blit(heart, (8 + i*50, 30))
 
     screen.blit(txt.render(score_txt, True, (255, 255, 255)),(5,5))
-
+    enemy_group.draw(screen)
+    enemy_group.update()
     ussike.draw(screen)
     ussike.update()
     verelible_group.draw(screen)
     verelible_group.update()
-
+    
     pygame.display.update()
 
 pygame.quit()
